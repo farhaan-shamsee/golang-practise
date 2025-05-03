b5e37d718eab8ecdfd49f2ed80457ad80f0269ef
farhaan.shamsee <farhaan.shamsee@siemens.com>
2025-05-03 02:02:32 +0000

    add initial implementation of pointer example in main.go and create go.mod file

---
+++ 07mypointers/main.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Intro to pointers")
+
+	// var ptr *int // ptr is a pointer to an int
+	// fmt.Println("Value of pointer is:", ptr)
+
+	myNumber := 23
+
+	var ptr = &myNumber // ptr is a pointer to myNumber , referencing the address of myNumber that is why we use & operator
+	fmt.Println("Value of pointer is:", ptr) // prints the address of myNumber
+	fmt.Println("Value of myNumber is:", *ptr) // dereferencing the pointer to get the value of myNumber
+
+	*ptr = *ptr + 2 // dereferencing the pointer to change the value of myNumber
+	fmt.Println("Value of myNumber is:", myNumber) // prints 25
+}
