13050728276add06bca7350dd19449c71895aee2
farhaan.shamsee <farhaan.shamsee@siemens.com>
2025-05-11 09:48:43 +0000

    feat: implement MongoDB API with CRUD operations for movie management

---
+++ 19mongoapi/controller/controller.go
@@ -0,0 +1,123 @@
+package controller
+
+import (
+	"context"
+	"fmt"
+	"log"
+
+	model "github.com/farhaan-shamsee/mongoapi/models"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/v2/bson"
+	"go.mongodb.org/mongo-driver/v2/mongo"
+	"go.mongodb.org/mongo-driver/v2/mongo/options"
+)
+
+const connectionString = "mongodb+srv://farrowaman:bcr5wsqTghksKEkc@cluster0.kaqhouk.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+const dbName = "netflix"
+const colName = "watchlist"
+
+// MOST IMPORTANT
+
+var collection *mongo.Collection
+
+// connect with mongo DB
+
+func init() {
+	// client options
+	clientOption := options.Client().ApplyURI(connectionString)
+
+	// connect to mongoDB
+	client, err := mongo.Connect(context.TODO(), clientOption)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("MOngoDB connection successful")
+
+	collection = client.Database(dbName).Collection(colName)
+
+	// collection instance
+	fmt.Println("Collection instance is ready")
+
+}
+
+// MONGODB helpers
+
+// insert 1 record
+
+func insertOneMovie(movie model.Netflix) {
+	inserted, err := collection.InsertOne(context.Background(), movie)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Inserted 1 movie in DB with ID: ", inserted.InsertedID)
+}
+
+// update one record
+func updateOneMovie(movieId string) {
+	id, err := primitive.ObjectIDFromHex(movieId)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	filter := bson.M{"_id": id}
+	update := bson.M{"$set": bson.M{"watched": true}}
+
+	result, err := collection.UpdateOne(context.Background(), filter, update)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("modified count: ", result.ModifiedCount)
+}
+
+// delete 1 record
+
+func deleteOneMovie(movieId string) {
+	id, err := primitive.ObjectIDFromHex(movieId)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	filter := bson.M{"_id": id}
+	deleteCount, err := collection.DeleteOne(context.Background(), filter)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Delete count: ", deleteCount)
+}
+
+// delete all records from MongoDB
+
+func deleteAllMovie() int64 {
+	filter := bson.M{{}}
+	deleteResult, err := collection.DeleteMany(context.Background(), filter)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Delete count: ", deleteResult.DeletedCount)
+	return deleteResult.DeletedCount
+}
+
+// get all movies from database
+func getAllMovies() []primitive.M {
+	cur, err := collection.Find(context.Background(), bson.M{{}})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var movies []primitive.M
+	for cur.Next(context.Background()) {
+		var movie bson.M
+		err := cur.Decode(&movie)
+		if err != nil {
+			log.Fatal(err)
+		}
+		movies = append(movies, movie)
+	}
+
+	defer cur.Close(context.Background())
+	return movies
+}
