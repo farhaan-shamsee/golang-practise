b8634bc393ef7a8dd0068f6a63864361141348c2
Farhaan Shamsee <77592635+farhaan-shamsee@users.noreply.github.com>
2025-05-31 13:09:34 +0530

    Create for-select-pattern.go

---
+++ for-select-pattern.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+
+	charChannel := make(chan string, 3)
+
+	chars := []string{"a", "b", "c"}
+
+	for _, char := range chars {
+		select {
+		case charChannel <- char:
+		}
+	}
+	close(charChannel)
+
+	for result := range charChannel {
+		fmt.Println(result)
+	}
+
+}
